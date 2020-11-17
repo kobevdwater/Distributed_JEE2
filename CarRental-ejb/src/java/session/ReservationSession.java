3a00beb1f632cf0f0f0eef247b2a7f778295c8ad
EclipseQuinten <47850723+EclipseQuinten@users.noreply.github.com>
2020-11-17 14:01:35 +0100

    veel functies, veel em.find

--- CarRental-ejb/src/java/session/ReservationSession.java
+++ CarRental-ejb/src/java/session/ReservationSession.java
@@ -10,6 +10,8 @@ import javax.ejb.Stateful;
 import javax.ejb.TransactionAttribute;
 import static javax.ejb.TransactionAttributeType.REQUIRED;
 import static javax.ejb.TransactionAttributeType.SUPPORTS;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transaction;
 import rental.CarRentalCompany;
 import rental.CarType;
@@ -27,28 +29,45 @@ public class ReservationSession implements ReservationSessionRemote {
     private EJBContext context;
     private String renter;
     private List<Quote> quotes = new LinkedList<Quote>();
+    
+    @PersistenceContext
+    private EntityManager em;
 
     @Override
     public Set<String> getAllRentalCompanies() {
-        return new HashSet<String>(RentalStore.getRentals().keySet());
+        //return new HashSet<String>(RentalStore.getRentals().keySet());
+        
+        
+        // Die maakt een list van crc's maar normaal gezien zou ik toch strings moeten krijgen? select c.name, dus uhhh
+        HashSet<String> result = new HashSet();
+        List<CarRentalCompany> out = em.createQuery("SELECT c.name FROM CarRentalCompany c", CarRentalCompany.class).getResultList();
+        for(CarRentalCompany crc : out) {
+            result.add(crc.getName());
+        }
+        return result;
     }
+    
     
     @Override
     public List<CarType> getAvailableCarTypes(Date start, Date end) {
         List<CarType> availableCarTypes = new LinkedList<CarType>();
         for(String crc : getAllRentalCompanies()) {
-            for(CarType ct : RentalStore.getRentals().get(crc).getAvailableCarTypes(start, end)) {
+            for(CarType ct : em.find(CarRentalCompany.class, crc).getAvailableCarTypes(start, end)) {
                 if(!availableCarTypes.contains(ct))
                     availableCarTypes.add(ct);
             }
         }
         return availableCarTypes;
     }
+    
+     public String getCheapestCarType(Date start, Date end, String region) {
+         
+     }
 
     @Override
     public Quote createQuote(String company, ReservationConstraints constraints) throws ReservationException {
         try {
-            Quote out = RentalStore.getRental(company).createQuote(constraints, renter);
+            Quote out = em.find(CarRentalCompany.class, company).createQuote(constraints, renter);
             quotes.add(out);
             return out;
         } catch(Exception e) {
@@ -59,7 +78,7 @@ public class ReservationSession implements ReservationSessionRemote {
     @Override
     public Quote createQuote(ReservationConstraints constraints) throws ReservationException {
         for (String crcName : getAllRentalCompanies()){
-            CarRentalCompany crc = RentalStore.getRental(crcName);
+            CarRentalCompany crc = em.find(CarRentalCompany.class, crcName);
             if (crc.canCreateQuote(constraints)){
                 Quote out = crc.createQuote(constraints, renter);
                 quotes.add(out);
@@ -80,11 +99,12 @@ public class ReservationSession implements ReservationSessionRemote {
         List<Reservation> done = new LinkedList<Reservation>();
         try {
             for (Quote quote : quotes) {
-                done.add(RentalStore.getRental(quote.getRentalCompany()).confirmQuote(quote));
+                //done.add(RentalStore.getRental(quote.getRentalCompany()).confirmQuote(quote));
+                done.add(em.find(CarRentalCompany.class, quote.getRentalCompany()).confirmQuote(quote));
             }
         } catch (Exception e) {
             for(Reservation r:done)
-                RentalStore.getRental(r.getRentalCompany()).cancelReservation(r);
+                em.find(CarRentalCompany.class, r.getRentalCompany()).cancelReservation(r);
             context.setRollbackOnly();
             throw new ReservationException(e);
         }
